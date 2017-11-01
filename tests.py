976a7b53cbcda6bac02acb231cef5c78b3b39811
a-hilaly <hilalyamine@gmail.com>
2017-11-01 12:49:07 +0100

    test

---
+++ tests.py
@@ -0,0 +1,33 @@
+from mpatterns import (
+    ALPHA_PATTERN,
+    NUMERIC_PATTERN,
+    ALPHANUMERIC_PATTERN,
+    DefaultException,
+    Validator,
+    PatternValidator,
+    ValidateDecorator,
+    NumericValidator,
+    NumericValidateDecorator,
+    AlphaValidator,
+    AlphaValidateDecorator,
+    AlphaNumericValidator,
+    AlphaNumericValidateDecorator,
+)
+
+def default_tests():
+    pass
+
+def alpha_tests():
+    pass
+
+def numeric_tests():
+    pass
+
+def alpha_numeric_tests():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    pass
